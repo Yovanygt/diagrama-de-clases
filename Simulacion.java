166c2c26baae95ca5e529362e22983f76fc4bcab
Yovanygt <143241191+Yovanygt@users.noreply.github.com>
2024-08-09 17:48:21 -0600

    Add files via upload
    5. Clase Simulacion
    Descripción:
    Simulacion es una clase que contiene métodos para realizar operaciones relacionadas con el equipo de fútbol. En este caso, tiene un método para imprimir los detalles de todos los miembros de un equipo.

---
+++ Simulacion.java
@@ -0,0 +1,11 @@
+
+
+public class Simulacion {
+
+    public void imprimirEquipo(Persona[] equipo) 
+    {
+        for (Persona persona : equipo) {
+            System.out.println(persona);
+        }
+    }
+}
